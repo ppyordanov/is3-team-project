1d74d483da7ed442c3119d3bc0070dc94e338292
Toni <rincerwind@gmail.com>
2013-11-30 15:22:51 +0000

    Adding the project skeleton classes and the UI classes

---
+++ IS3System/src/is3system/IS3System.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is3system;
+
+import userInterface.IS3SystemUI;
+
+/**
+ *
+ * @author Samsung
+ */
+public class IS3System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IS3SystemUI theUI = new IS3SystemUI();
+        theUI.setVisible(true);
+    }
+}
